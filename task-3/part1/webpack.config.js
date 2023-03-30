dd4bb3eeef8d483adb87a33e612ac202fa492f0a
Pavel Shpakovich <pavel.shpakovich@inspirebrands.com>
2023-03-30 17:37:48 +0300

    [TASK-3]

--- task-3/part1/webpack.config.js
+++ task-3/part1/webpack.config.js
@@ -1,30 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'images',
-                },
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images',
+        },
+      },
     ],
-    mode: 'production'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+  mode: 'production',
 };
--- task-3/part2/webpack.config.js
+++ task-3/part2/webpack.config.js
@@ -1,30 +1,40 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const modernConfig = require('./babel.modern.config');
+const legacyConfig = require('./babel.legacy.config');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'images',
-                },
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: process.env.BUILD_MODERN === 'true' ? 'modern.bundle.js' : 'legacy.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: process.env.BUILD_MODERN === 'true' ? modernConfig : legacyConfig,
+      },
     ],
-    mode: 'production'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+  mode: 'production',
 };
